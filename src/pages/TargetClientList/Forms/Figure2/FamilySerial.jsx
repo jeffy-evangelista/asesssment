f0a6131fcb8321d5617e9733d8ff42ed3713b3cb
eiroljan <eiroljan19@gmail.com>
2021-11-28 08:26:41 +0800

    changes

--- src/pages/TargetClientList/Forms/Figure2/FamilySerial.jsx
+++ src/pages/TargetClientList/Forms/Figure2/FamilySerial.jsx
@@ -11,10 +11,64 @@ import {
     NumberDecrementStepper,
     Select,
     InputLeftAddon,
-    InputGroup
+    InputGroup,
+    Button
 } from '@chakra-ui/react'
+import {
+    collection,
+    getDocs,
+    addDoc,
+  } from "firebase/firestore"
+  import { db } from '../../../../utils/init-firebase'
+  import  {useState} from "react";
+
 
 export default function FamilySerial() {
+    
+    const [newFirstName, setNewFirstName] = useState("");
+    const [newMiddleName, setNewMiddleName] = useState("");
+    const [newLastName, setNewLastName] = useState("");
+    const [newOccupation, setNewOccupation] = useState("");
+    const [newStreet, setNewStreet] = useState("");
+    const [newBarangay, setNewBarangay] = useState("");
+    const [newMunisipal, setNewMunisipal] = useState("");
+    const [newProvince, setNewProvince] = useState("");
+    const [newSpouse, setNewSpouse] = useState("");
+    const [newFirst, setNewFirst] = useState("");
+    const [newMiddle, setNewMiddle] = useState("");
+    const [newLast, setNewLast] = useState("");
+    const [newOccup, setNewOccup] = useState("");
+    const [newEduc, setNewEduc] = useState("");
+    const [newEducto, setNewEducto] = useState("");
+    
+
+    const usersCollectionRef = collection(db, "family");
+    
+    const createFamily = async () => {
+        await addDoc(usersCollectionRef, { 
+            firstname: newFirstName, 
+            middlename: newMiddleName, 
+            lastname: newLastName,
+            occupation: newOccupation,
+            street: newStreet,
+            barangay: newBarangay,
+            munisipal: newMunisipal,
+            province: newProvince,
+            spouse: newSpouse,
+            first: newFirst,
+            middle: newMiddle,
+            last: newLast,
+            occu: newOccup,
+            education: newEduc,
+            educationto: newEducto,
+            
+        });
+      };
+    
+  
+
+
+
 
     return (
         <>
@@ -25,21 +79,30 @@ export default function FamilySerial() {
             <GridItem colSpan={4}>
                 <FormControl>
                     <FormLabel>First Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text" 
+                    onChange={(event) => {
+                    setNewFirstName(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Middle Name */}
             <GridItem colSpan={4}>
                 <FormControl>
                     <FormLabel>Middle Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewMiddleName(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Last Name */}
             <GridItem colSpan={4}>
                 <FormControl>
                     <FormLabel>Last Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewLastName(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Birthday */}
@@ -53,7 +116,7 @@ export default function FamilySerial() {
             <GridItem colSpan={3}>
                 <FormControl>
                     <FormLabel>Age</FormLabel>
-                    <NumberInput allowMouseWheel defaultValue={10} min={10} max={100}>
+                    <NumberInput allowMouseWheel defaultValue={10} min={10} max={100} >
                         <NumberInputField />
                         <NumberInputStepper>
                             <NumberIncrementStepper />
@@ -66,7 +129,9 @@ export default function FamilySerial() {
             <GridItem colSpan={3}>
                 <FormControl id="education">
                     <FormLabel>Highest Education</FormLabel>
-                    <Select placeholder="Select">
+                    <Select placeholder="Select" onChange={(event) => {
+                    setNewEduc(event.target.value);
+                    }}>
                         <option>Elementary</option>
                         <option>High School</option>
                         <option>College</option>
@@ -77,35 +142,50 @@ export default function FamilySerial() {
             <GridItem colSpan={3}>
                 <FormControl>
                     <FormLabel>Occupation</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewOccupation(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* No. Street */}
             <GridItem colSpan={3}>
                 <FormControl>
                     <FormLabel>No. Street</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewStreet(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Barangay */}
             <GridItem colSpan={3}>
                 <FormControl>
                     <FormLabel>Barangay</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewBarangay(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Municipality */}
             <GridItem colSpan={3}>
                 <FormControl>
                     <FormLabel>Municipality</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewMunisipal(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Province */}
             <GridItem colSpan={3}>
                 <FormControl>
                     <FormLabel>Province</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewProvince(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             <GridItem colSpan={12}>
@@ -115,21 +195,30 @@ export default function FamilySerial() {
             <GridItem colSpan={4}>
                 <FormControl>
                     <FormLabel>First Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewFirst(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Middle Name */}
             <GridItem colSpan={4}>
                 <FormControl>
                     <FormLabel>Middle Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewMiddle(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Last Name */}
             <GridItem colSpan={4}>
                 <FormControl>
                     <FormLabel>Last Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewLast(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             {/* Birthday */}
@@ -143,7 +232,9 @@ export default function FamilySerial() {
             <GridItem colSpan={4}>
                 <FormControl id="education">
                     <FormLabel>Highest Education</FormLabel>
-                    <Select placeholder="Select" >
+                    <Select placeholder="Select" onChange={(event) => {
+                    setNewEducto(event.target.value);
+                    }}>
                         <option>Elementary</option>
                         <option>High School</option>
                         <option>College</option>
@@ -153,7 +244,10 @@ export default function FamilySerial() {
             <GridItem colSpan={4}>
                 <FormControl>
                     <FormLabel>Occupation</FormLabel>
-                    <Input type="text" />
+                    <Input type="text"  
+                    onChange={(event) => {
+                    setNewOccup(event.target.value);
+                    }}/>
                 </FormControl>
             </GridItem>
             <GridItem colSpan={6}>
@@ -170,6 +264,9 @@ export default function FamilySerial() {
                         </NumberInput>
                     </InputGroup>
                 </FormControl>
+                <Button colorScheme='green' w='full' onClick={createFamily}>
+              Submit
+            </Button>
             </GridItem>
         </>
     )
