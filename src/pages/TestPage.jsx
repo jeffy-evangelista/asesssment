f389e442d3baf94d7a1a57a81b775f8e8b2b56c8
Gene Gulanes Jr <ggulanes_180000000405@uic.edu.ph>
2021-12-09 18:03:08 +0800

    cleaner test page and modal

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,3 +1,6 @@
+
+
+import AnimeList from './modal';
 import {
     Flex,
     Spacer,
@@ -6,22 +9,11 @@ import {
     Heading,
     Table,
     Thead,
-    Tr, useToast,
+    Tr,
     Th,
     Tbody,
     Td,
-    useDisclosure,
-    Drawer,
-    DrawerBody,
-    Checkbox, CheckboxGroup,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,
-    DrawerCloseButton,
-    FormLabel,Input, FormControl, FormErrorMessage,
 } from '@chakra-ui/react'
-import { useForm } from "react-hook-form";
 import {
     ViewIcon,
     AddIcon } from '@chakra-ui/icons'
@@ -29,31 +21,16 @@ import { Layout } from '../components/Layout'
 import { Link } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
 import {
-    addDoc,
     collection,
-    getDocs, updateDoc,doc
+    getDocs,
 } from "firebase/firestore";
-import { db} from '../utils/init-firebase'
-import {Formik,Form,Field} from "formik";
+import { db } from '../utils/init-firebase'
 
 
-export default function TestPage() {
+export default function SocialWorker() {
 
     const [socialWork, setSocialWork] = useState([]);
-    const [data1, data2] = useState([]);
     const usersCollectionRef = collection(db, "users");
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const btnRef = React.useRef()
-    const firstField = React.useRef()
-
-
-
-
-
-
-
-    const toast = useToast( )
-
 
     useEffect(() => {
         const getSocialWork = async () => {
@@ -66,7 +43,8 @@ export default function TestPage() {
 
 
 
-        return (
+
+    return (
 
 
         <Layout>
@@ -93,140 +71,18 @@ export default function TestPage() {
 
                         return (
 
-                            <Tr >
-                                <Td key={works.id}>{works.displayName}</Td>
+                            <Tr key={works.id}>
+                                <Td>{works.displayName}</Td>
                                 <Td>
                                     <Stack direction="row" spacing={1}>
-                                        <Button   ref={btnRef} colorScheme='teal'   onClick={() => {
-                                            onOpen();
-
-                                        }} >
-
-                                            <ViewIcon/>
-                                        </Button>
+                                            <AnimeList colorScheme="teal"/>
                                     </Stack>
                                 </Td>
                             </Tr>
-
-
-
                         );
-
-
                     })}
                 </Tbody>
-
             </Table>
-            <Drawer
-                isOpen={isOpen}
-                placement='right'
-                initialFocusRef={firstField}
-                onClose={onClose}
-            >
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerCloseButton />
-                    <DrawerHeader borderBottomWidth='1px'>
-                      Update Social Worker
-                    </DrawerHeader>
-
-                    <DrawerBody>
-                        <Stack spacing='24px'>
-                            <Formik
-                                initialValues={{  }}
-                                onSubmit={(values, actions) => {
-                                    setTimeout(() => {
-                                        alert(JSON.stringify(values, null, 2))
-                                        actions.setSubmitting(false)
-                                    }, 1000)
-                                }}
-                            >
-                                {(props) => (
-                                    <Form>
-                                        <Field name='displayName' >
-                                            {({ field, form }) => (
-                                                <FormControl isInvalid={form.errors.displayName && form.touched.displayName}>
-                                                    <FormLabel htmlFor='displayName'>Display Name</FormLabel>
-                                                    <Input {...field} id='displayName' placeholder='displayName' />
-                                                    <FormErrorMessage>{form.errors.displayName}</FormErrorMessage>
-                                                </FormControl>
-                                            )}
-                                        </Field>
-
-                                        <Field name='email' >
-                                            {({ field, form }) => (
-                                                <FormControl isInvalid={form.errors.email && form.touched.email}>
-                                                    <FormLabel htmlFor='email'>Email Address</FormLabel>
-                                                    <Input {...field} id='email' placeholder='email' />
-                                                    <FormErrorMessage>{form.errors.email}</FormErrorMessage>
-                                                </FormControl>
-                                            )}
-                                        </Field>
-                                        <FormControl >
-                                        <Field as="select" name="legislativeDistrict">
-                                            <option value="red">D1</option>
-                                            <option value="green">D2</option>
-                                            <option value="blue">D3</option>
-
-                                        </Field></FormControl>
-
-                                            <FormControl >
-                                        <Field as="select" name="administrativeDistrict" >
-
-                                            <option value="red">Paquibato</option>
-                                            <option value="red2">Paquibato2</option>
-
-                                        </Field></FormControl>
-                                                <FormControl >
-                                        <Field as="select" name="barangay">
-                                            <option value="col">Colosas</option>
-                                            <option value="fat">Fatima (Benowang)</option>
-                                            <option value="lum"> Lumiad</option>
-                                            <option value="mab">Mabuhay</option>
-                                            <option value="mal">  Malabog</option>
-                                            <option value="map"> Mapula</option>
-                                            <option value="pan"> Panalum</option>
-                                            <option value="pand">  Pandaitan</option>
-                                            <option value="paq">  Paquibato Proper</option>
-                                            <option value="pare">  Paradise Embak</option>
-                                            <option value="sal">  Salapawan</option>
-                                            <option value="sumi"> Sumimao</option>
-                                            <option value="tap"> Tapak</option>
-
-                                        </Field></FormControl>
-                                                    <FormControl >
-                                        <Field as="select" name="isAdmin" >
-
-                                            <option value="true">isAdmin</option>
-                                            <option value="false">isNotAdmin</option>
-
-                                        </Field>
-                                                    </FormControl>
-                                        <Button
-                                            mt={4}
-                                            colorScheme='teal'
-                                            isLoading={props.isSubmitting}
-                                            type='submit'
-                                        >
-                                            Submit
-                                        </Button>
-                                    </Form>
-                                )}
-                            </Formik>
-                        </Stack>
-                    </DrawerBody>
-
-                    <DrawerFooter borderTopWidth='1px'>
-                        <Button variant='outline' mr={3} onClick={onClose}>
-                            Cancel
-                        </Button>
-
-
-                    </DrawerFooter>
-                </DrawerContent>
-            </Drawer>
-
-
         </Layout>
 
     )
